7da7c3d04169eaa246bf7bdeb0d9f875f966817e
Luciano Adassus <luciano106@gmail.com>
2025-02-18 13:23:38 -0300

    endpint to get token test update doc
    Signed-off-by: Luciano Adassus <luciano106@gmail.com>

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -14,30 +14,31 @@ ENVIRONMENT = os.getenv("ENVIRONMENT", "local")  # Default to local
 fake_user_db = {"user": {"username": "user", "password": "password"}}
 
 if ENVIRONMENT == "local":
-    @router.post("/token", summary="Generate JWT Token", description="""
-    This endpoint generates a **JWT access token** for authentication.
-
-    ### How it works:
-    1. In Swagger UI (`/docs`):
-    - Click "Authorize" (🔒).
-    - Enter your `username` and `password` (e.g., `user` / `password`).
-    - Swagger will automatically retrieve the token and use it for secured endpoints.
-
-    2. Public Endpoint:
-    - This endpoint does **not** require authentication.
-    - It is **publicly accessible** for users to obtain an authentication token to test.
-
-    3. Using the Token in REST Clients (Postman, cURL, etc.):
-    - Make a `POST /token` request to retrieve a token.
-    - Use the token in the `Authorization` header for protected requests:
-        ```bash
-        curl -X GET "http://localhost:8000/movies/search?title=Sun" \
-        -H "Authorization: Bearer eyJhbGciOiJIUzI..."
-        ```
-
-    Once authenticated, you can access all protected endpoints without manually passing the token in Swagger UI.
-    """)
+    @router.post("/token", summary="Generate JWT Token")
     def login(form_data: OAuth2PasswordRequestForm = Depends()):
+        """
+        This endpoint generates a **JWT access token** for authentication.
+
+        How it works:
+        1. In Swagger UI (`/docs`):
+        - Click "Authorize" (🔒).
+        - Enter your `username` = user and `password` = password.
+        - Swagger will automatically retrieve the token and use it for secured endpoints.
+
+        2. Public Endpoint:
+        - This endpoint does **not** require authentication.
+        - It is **publicly accessible** for users to obtain an authentication token to test.
+
+        3. Using the Token in REST Clients (Postman, cURL, etc.):
+        - Make a `POST /token` request to retrieve a token, same credentials point 1 above.
+        - Use the token in the `Authorization` header for protected requests:
+            ```bash
+            curl -X GET "http://localhost:8000/movies/search?title=Sun" \
+            -H "Authorization: Bearer eyJhbGciOiJIUzI..."
+            ```
+
+        Once authenticated, you can access all protected endpoints without manually passing the token in Swagger UI.
+        """
         user = fake_user_db.get(form_data.username)
         if not user or form_data.password != user["password"]:
             raise HTTPException(status_code=401, detail="Invalid credentials")
